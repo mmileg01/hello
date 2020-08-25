3c0eb5e89b86834726c9e022e21f393ba12f8c23
mmileg01 <69811877+mmileg01@users.noreply.github.com>
2020-08-24 21:11:13 -0500

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,18 @@
+/**
+ * Author: Malaki Mileg
+ *         mmileg2@huskers.unl.edu
+ * Date: 2020/03/26
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Malaki Mileg!\n");
+  printf("Computer Science\n");
+
+  return 0;
+}
